1ece0c83898f277e0238268daf76797fb6f88b54
Matt2844 <matt.hd.ford@gmail.com>
2021-01-02 22:55:24 -0500

    preview form styling done, now working on the show/hide buttons

--- pet-report/src/Form.js
+++ pet-report/src/Form.js
@@ -3,6 +3,8 @@ import React, { useState } from 'react'
 
 
 export default function Form () {
+
+  // Setting the state for the preview form. 
   const [companyName, setCompanyName] = useState('Company Name');
   const [petName, setPetName] = useState('Pet');
   const [date, setDate] = useState('Date(s)');
@@ -12,14 +14,24 @@ export default function Form () {
   const [sociability, setSociability] = useState('Very Sociable');
   const [cooperation, setCooperation] = useState('Very Cooperative');
   const [grade, setGrade] = useState('A+');
+  const [notes, setNotes] = useState(`EXAMPLE PLACEHOLDER: It was so much fun to look after Spot! He behaved very well. He lost his ball at the park, but we were able to get him a new one. This box can be extended my manually dragging down the corner.`)
+
+  // Setting the state for the hide and show buttons. 
+  const [show, setShow] = useState(true)
+
 
   return (
 
     <div className="form-container">
       <div className="form-preview">
-        <h3>{companyName}</h3>
+
+        {show === true ? (
+          <h3>{companyName}</h3>
+        ) : (
+            <h3>Pet Report Pro</h3>
+          )}
         <div className="forms">
-          <p className="form-pet-name">{petName}'s report card for the period of <br />{date}</p>
+          <p className="form-pet-name" >{petName}'s report card for the period of <br />{date}</p>
           <div className="report-grid">
             <hr />
             <div className="report-walks">Total Number of Walks:<span>{walks}</span>
@@ -36,7 +48,14 @@ export default function Form () {
             <div className="report-cooperation">Cooperation:<span>{cooperation}</span>
               <hr />
             </div>
-            <div className="report-grade">Overall Grade:<span>{grade}</span><hr /></div>
+            <div className="report-grade">Overall Grade:<span>{grade}</span>
+              <hr />
+            </div>
+            <div className="report-notes">
+              <div className="report-notes-title">Caregiver Notes:</div>
+              <textarea className="report-notes-input" rows="4" cols="52" value={notes}>{notes}</textarea>
+            </div>
+
           </div>
         </div>
       </div>
@@ -49,17 +68,17 @@ export default function Form () {
 
           <div className="input-container">
             <label>Company Name:</label><br />
-            <input className="company-name" onChange={event => setCompanyName(event.target.value)} type="text" placeholder="ex. Ford Dog Walking Inc."></input><button>Hide</button><button>Show</button><br />
+            <input className="company-name" onChange={event => setCompanyName(event.target.value)} type="text" placeholder="ex. Ford Dog Walking Inc."></input><button onClick={() => setShow(!show)}>Show / Hide</button><br />
           </div>
 
           <div className="input-container">
             <label>Pet Name:</label><br />
-            <input className="pet-name" onChange={event => setPetName(event.target.value)} type="text" placeholder="ex. Spot"></input><button>Hide</button><button>Show</button><br />
+            <input className="pet-name" onChange={event => setPetName(event.target.value)} type="text" placeholder="ex. Spot"></input><span>Required</span><br />
           </div>
 
           <div className="input-container">
             <label>Date(s):</label><br />
-            <input className="date" onChange={event => setDate(event.target.value)} type="text" placeholder="ex. Dec 5-10 or just 'Dec 5'"></input><button>Hide</button><button>Show</button><br />
+            <input className="date" onChange={event => setDate(event.target.value)} type="text" placeholder="ex. Dec 5-10 or just 'Dec 5'"></input><span>Required</span><br />
           </div>
 
           <div className="input-container">
@@ -122,10 +141,10 @@ export default function Form () {
           </div>
 
           <div className="input-container">
-            <label for="w3review">Teachers Notes:</label>
-            <textarea className="teachers-notes" rows="4" cols="50">
-              (pet name) was a lot of fun to look after!
-          </textarea><br />
+            <label>Caregiver Notes:</label>
+            <textarea className="teachers-notes" rows="4" cols="50" onChange={event => setNotes(event.target.value)} >
+
+            </textarea><br />
           </div>
 
         </div>
