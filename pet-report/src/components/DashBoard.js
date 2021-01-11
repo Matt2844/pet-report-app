bac0a1c5eb02319497d192699b67a176a4bbee28
Matt2844 <matt.hd.ford@gmail.com>
2021-01-11 00:05:50 -0500

    added the boarding theme

--- pet-report/src/components/DashBoard.js
+++ pet-report/src/components/DashBoard.js
@@ -12,11 +12,14 @@ import SettingsIcon from '@material-ui/icons/Settings';
 
 export default function DashBoard () {
   {/* Theme State */ }
+  const [selectTheme, setSelectTheme] = useState(true)
   const [basicTheme, setBasicTheme] = useState(false)
+  const [boardingTheme, setBoardingTheme] = useState(false)
   {/* Form State Set by the Form Editor */ }
-  const [companyName, setCompanyName] = useState('Company Name')
-  const [petName, setPetName] = useState('Example Spot')
-  const [date, setDate] = useState('Date Example - Date Example')
+  const [companyName, setCompanyName] = useState('[Company Name]')
+  const [petName, setPetName] = useState('[Pet Name]')
+  const [date, setDate] = useState('[Date Example - Date Example]')
+  {/* Basic Theme */ }
   const [walks, setWalks] = useState('')
   const [outsideTime, setOutsideTime] = useState('')
   const [appetite, setAppetite] = useState('Ate Everything')
@@ -24,9 +27,15 @@ export default function DashBoard () {
   const [behaviour, setBehaviour] = useState('Very Well Behaved')
   const [grade, setGrade] = useState('A+')
   const [notes, setNotes] = useState('To edit notes, use the panel to the right.')
+  {/* Boarding Theme */ }
+  const [gender, setGender] = useState('Their')
+  const [boardingFriends, setBoardingFriends] = useState('[Pet Friends]')
+  const [boardingToy, setBoardingToy] = useState('[Toy]')
+  const [boardingActivity, setBoardingActivity] = useState('[Activity]')
 
   {/* Form State Set by the Hide or Show Buttons */ }
   const [showCompany, setShowCompany] = useState(true)
+  {/* Basic Theme */ }
   const [showWalks, setShowWalks] = useState(true)
   const [showOutsideTime, setShowOutsideTime] = useState(true)
   const [showAppetite, setShowAppetite] = useState(true)
@@ -34,16 +43,25 @@ export default function DashBoard () {
   const [showBehaviour, setShowBehaviour] = useState(true)
   const [showGrade, setShowGrade] = useState(true)
   const [showNotes, setShowNotes] = useState(true)
+  {/* Boarding Theme */ }
+  const [showBoardingFriends, setShowBoardingFriends] = useState(true)
+  const [showFavoriteToy, setShowFavoriteToy] = useState(true)
+  const [showBoardingActivity, setShowBoardingActivity] = useState(true)
 
 
 
 
   // reads what theme it is and sets state accordingly
   const findTheme = (theme) => {
+    setSelectTheme(false);
+
     if (theme === 'Basic') {
       setBasicTheme(true)
     } else if (theme !== 'Basic') {
       setBasicTheme(false)
+    }
+    if (theme === 'Boarding') {
+      setBoardingTheme(true)
     }
   }
 
@@ -131,6 +149,17 @@ export default function DashBoard () {
             <span>{date}</span>
           </div>
 
+          {/* SELECT A THEME, THEME */}
+          {selectTheme === true ? (
+            <div className="report-data-section">
+              <div className="report-data-col1">
+                <p className="get-started">Select a theme to get started</p>
+              </div>
+            </div>
+          ) : null}
+
+
+          {/* BASIC THEME */}
           {basicTheme === true ? (
             <div className="report-data-section">
               <div className="report-data-col1">
@@ -160,7 +189,34 @@ export default function DashBoard () {
                 ) : null}
               </div>
             </div>
-          ) : (<p className="get-started">Select a theme to get started.</p>)}
+          ) : null}
+
+          {/* BOARDING THEME */}
+          {boardingTheme === true ? (
+            <div className="report-data-section">
+              <div className="report-data-col1">
+                {showBoardingFriends === true ? (
+                  <div className="pet-friends">{petName} liked to play with {boardingFriends}.</div>
+                ) : null}
+                {showFavoriteToy === true ? (
+                  <div className="pet-activity">{gender} favorite toy was the {boardingToy}.</div>
+                ) : null}
+                {showBoardingActivity === true ? (
+                  <div className="pet-excitement">{petName} really enjoyed {boardingActivity}.</div>
+                ) : null}
+                {showGrade === true ? (
+                  <div className="overall-grade">Overall Grade: <span>{grade}</span></div>
+                ) : null}
+                {showNotes === true ? (
+                  <div>
+                    <div className="notes">Notes:</div>
+                    <textarea className="notes-input" rows="14" cols="67" value={notes}></textarea>
+                  </div>
+                ) : null}
+              </div>
+            </div>
+          ) : null}
+
         </div>
       </div>
 
@@ -183,49 +239,98 @@ export default function DashBoard () {
             </select>
           </div>
 
-          <div className="input-container">
-            <label>Total Walks:</label>
-            <input className="total-walks" onChange={event => setWalks(event.target.value)} type="text"></input>
-            <button onClick={() => setShowWalks(!showWalks)}>{showWalks ? "x" : "show"} </button>
-          </div>
+          {basicTheme === true ? (
 
-          <div className="input-container">
-            <label className="time-outside-text">Est. Time Outside Per Day:</label>
-            <input className="time-outside-day" onChange={event => setOutsideTime(event.target.value)} type="text">
-            </input><button onClick={() => setShowOutsideTime(!showOutsideTime)}>{showOutsideTime ? "x" : "show"}</button>
-          </div>
+            <section>
+              <div className="input-container">
+                <label>Total Walks:</label>
+                <input className="total-walks" onChange={event => setWalks(event.target.value)} type="text"></input>
+                <button onClick={() => setShowWalks(!showWalks)}>{showWalks ? "x" : "show"} </button>
+              </div>
 
-          <div className="input-container">
-            <label>Appetite:</label><br />
-            <select className="appetite" onChange={event => setAppetite(event.target.value)}>
-              <option value="Ate Everything" selected>Ate Everything</option>
-              <option value="Ate Almost Everything">Ate Almost Everything</option>
-              <option value="Some Issues with Appetite">Issues With Appetite</option>
-              <option value="Barely Ate">Barely Ate</option>
-            </select>
-            <button onClick={() => setShowAppetite(!showAppetite)}>{showAppetite ? "x" : "show"}</button>
-          </div>
+              <div className="input-container">
+                <label className="time-outside-text">Est. Time Outside Per Day:</label>
+                <input className="time-outside-day" onChange={event => setOutsideTime(event.target.value)} type="text">
+                </input><button onClick={() => setShowOutsideTime(!showOutsideTime)}>{showOutsideTime ? "x" : "show"}</button>
+              </div>
 
-          <div className="input-container">
-            <label>Sociability:</label><br />
-            <select className="sociability" onChange={event => setSociability(event.target.value)}>
-              <option value="Very Sociable" selected>Very Sociable</option>
-              <option value="Sociable">Sociable</option>
-              <option value="Kept to Themselves">Kept to Themselves</option>
-            </select>
-            <button onClick={() => setShowSociability(!showSociability)}>{showSociability ? "x" : "show"}</button>
-          </div>
+              <div className="input-container">
+                <label>Appetite:</label><br />
+                <select className="appetite" onChange={event => setAppetite(event.target.value)}>
+                  <option value="Ate Everything" selected>Ate Everything</option>
+                  <option value="Ate Almost Everything">Ate Almost Everything</option>
+                  <option value="Some Issues with Appetite">Issues With Appetite</option>
+                  <option value="Barely Ate">Barely Ate</option>
+                </select>
+                <button onClick={() => setShowAppetite(!showAppetite)}>{showAppetite ? "x" : "show"}</button>
+              </div>
 
-          <div className="input-container">
-            <label>Behaviour:</label><br />
-            <select className="behaviour" onChange={event => setBehaviour(event.target.value)}>
-              <option value="Very Cooperative" selected>Very Well Behaved</option>
-              <option value="Cooperative">Well Behaved</option>
-              <option value="Fussy">Fussy</option>
-              <option value="Needs More Training">Needs More Training</option>
-            </select>
-            <button onClick={() => setShowBehaviour(!showBehaviour)}>{showBehaviour ? "x" : "show"}</button>
-          </div>
+              <div className="input-container">
+                <label>Sociability:</label><br />
+                <select className="sociability" onChange={event => setSociability(event.target.value)}>
+                  <option value="Very Sociable" selected>Very Sociable</option>
+                  <option value="Sociable">Sociable</option>
+                  <option value="Kept to Themselves">Kept to Themselves</option>
+                </select>
+                <button onClick={() => setShowSociability(!showSociability)}>{showSociability ? "x" : "show"}</button>
+              </div>
+
+              <div className="input-container">
+                <label>Behaviour:</label><br />
+                <select className="behaviour" onChange={event => setBehaviour(event.target.value)}>
+                  <option value="Very Cooperative" selected>Very Well Behaved</option>
+                  <option value="Cooperative">Well Behaved</option>
+                  <option value="Fussy">Fussy</option>
+                  <option value="Needs More Training">Needs More Training</option>
+                </select>
+                <button onClick={() => setShowBehaviour(!showBehaviour)}>{showBehaviour ? "x" : "show"}</button>
+              </div>
+            </section>
+
+          ) : null}
+
+          {boardingTheme === true ? (
+
+            <section>
+              <div className="input-container">
+                <label>Gender:</label>
+                <select className="pet-gender clean-label" onChange={event => setGender(event.target.value)}>
+                  <option value="His">Male</option>
+                  <option value="Her">Female</option>
+                  <option value="Their" selected>Select</option>
+                </select>
+              </div>
+
+              <div className="input-container">
+                <label>Friends:</label>
+                <input className="friends" onChange={event => setBoardingFriends(event.target.value)} type="text"></input>
+                <button onClick={() => setShowBoardingFriends(!showBoardingFriends)}>{showBoardingFriends ? "x" : "show"} </button>
+              </div>
+
+              <div className="input-container">
+                <label>Favority Toy:</label>
+                <input className="favorite-toy" onChange={event => setBoardingToy(event.target.value)} type="text"></input>
+                <button onClick={() => setShowFavoriteToy(!showFavoriteToy)}>{showFavoriteToy ? "x" : "show"} </button>
+              </div>
+
+              <div className="input-container">
+                <label>Favorite Activity:</label>
+                <select className="pet-boarding-activity clean-label" onChange={event => setBoardingActivity(event.target.value)}>
+                  <option value="[Select an Activity]" selected>Select</option>
+                  <option value="playing with staff">Playing w Staff</option>
+                  <option value="playing outside">Playing Outside</option>
+                  <option value="playing with others">Playing w Others</option>
+                  <option value="nap time">Nap Time</option>
+                  <option value="meal time">Meal Time</option>
+                  <option value="playing fetch">Playing Fetch</option>
+                  <option value="playing Tug-O-War">Playing Tug-O-War</option>
+                </select>
+                <button onClick={() => setShowBoardingActivity(!setShowBoardingActivity)}>{showBoardingActivity ? "x" : "show"} </button>
+              </div>
+
+            </section>
+
+          ) : null}
 
           <div className="input-container">
             <label>Overall Grade:</label>
@@ -252,11 +357,13 @@ export default function DashBoard () {
             </textarea>
           </div>
 
-
-
-
-
         </div>
+
+
+
+
+
+
 
         <div className="menu-col-2">
 
@@ -267,12 +374,12 @@ export default function DashBoard () {
 
           <div className="input-container">
             <label>Pet Name:</label><br />
-            <input className="pet-name" onChange={event => setPetName(event.target.value)} type="text" ></input><span className="name-required">Required</span><br />
+            <input className="pet-name" onChange={event => setPetName(event.target.value)} type="text" ></input><span className="name-required">Required</span>
           </div>
 
           <div className="input-container">
             <label>Date(s):</label><br />
-            <input className="date" onChange={event => setDate(event.target.value)} type="text" ></input><span className="date-required">Required</span><br />
+            <input className="date" onChange={event => setDate(event.target.value)} type="text" ></input><span className="date-required">Required</span>
           </div>
 
           <div className="input-container">
