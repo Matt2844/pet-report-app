bba298daf5aad432f0fd6df60d4ce16f9edd483b
Matt2844 <matt.hd.ford@gmail.com>
2021-02-27 18:26:07 -0500

    saving progress on about page

--- pet-report/src/components/About.js
+++ pet-report/src/components/About.js
@@ -1,10 +1,23 @@
 import React from 'react'
+import './About.css'
 
 export default function About () {
 
   return (
     <div>
-      About Page
+      <h3>About Pet Report Pro</h3>
+      <section className="about-intro">
+        <div className="about-intro-text" style={{
+          backgroundImage: `url("/images/pr-pro-abstract-background.jpg")`
+        }}>
+          <h4>Gen pet report cards for pet owners</h4>
+          <p>Establish trust</p>
+        </div>
+        <div className="about-intro-photo" style={{
+          backgroundImage: `url("/images/pr-pro-about-cat.jpg")`
+        }}>cat photo</div>
+      </section>
+      <p>Watch The Demo</p>
     </div>
   )
 }
