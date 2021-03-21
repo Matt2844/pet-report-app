51e021fdb97fcda8412651112567b7dea692fb22
Matt2844 <matt.hd.ford@gmail.com>
2021-03-21 15:48:37 -0400

    added donate page (complete)

--- pet-report/src/components/Donate.js
+++ pet-report/src/components/Donate.js
@@ -1,12 +1,27 @@
 import React from 'react'
+import Button from '@material-ui/core/Button';
+import { makeStyles } from '@material-ui/core/styles';
 
+
+const useStyles = makeStyles({
+  support: {
+    marginTop: '2rem',
+    backgroundColor: 'rgb(147, 207, 244)',
+  }
+})
 
 export default function Donate () {
-
+  const classes = useStyles();
 
   return (
-    <div>
-      <p>This is the donate page</p>
-    </div>
+    <div className="donate-wrapper">
+      <h3>Like The App?</h3>
+      <p>Hopefully this application adds value to your business. If it does, please support the site <a href='https://www.buymeacoffee.com/pet.report.pro' target="_blank" rel="noopener noreferrer">here</a> or by clicking 'Support' below.</p>
+      <p>Donations are collected through a site called Buy Me A Coffee. It is a friendly, fun, and secure way to make payments.</p>
+      <p>With enough donations, significant improvements will be made to Pet Report Pro. Improvements will be based on user input and feedback.</p>
+      <a href='https://www.buymeacoffee.com/pet.report.pro' target="_blank" rel="noopener noreferrer">
+        <Button className={classes.support}>Support</Button>
+      </a>
+    </div >
   )
 }
